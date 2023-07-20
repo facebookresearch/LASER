296656b8c86a7a4b076dabfdcba9f1113e593415
CaptainVee <captainvee3@gmail.com>
2023-07-20 12:30:22 +0100

    added header comment

--- laser_encoders/test_laser_tokenizer.py
+++ laser_encoders/test_laser_tokenizer.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# LASER  Language-Agnostic SEntence Representations
+# is a toolkit to calculate multilingual sentence embeddings
+# and to use them for document classification, bitext filtering
+# and mining
+#
+# --------------------------------------------------------
+# Tests for LaserTokenizer
+
 import os
 import pytest
 from pathlib import Path
