67b4f4aa4b97db45a72cea313fff1d5fd155d012
CaptainVee <captainvee3@gmail.com>
2023-08-04 16:16:51 +0100

    added language list and made some changes to the download models

--- laser_encoders/download_models.py
+++ laser_encoders/download_models.py
@@ -16,62 +16,81 @@
 
 # default to download to current directory
 
-import os
-import requests
+import argparse
 import logging
+import os
+import sys
 
-logger = logging.getLogger(__name__)
+import requests
+from language_list import LANGUAGE_MAPPING, SPM_LANGUAGE
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+logger = logging.getLogger("preprocess")
 
 class LaserModelDownloader:
     def __init__(self, model_dir):
         self.model_dir = model_dir
-        self.url = "https://dl.fbaipublicfiles.com/nllb/laser"
+        self.url = "https://dl.fbaipublicfiles.com/nllb/laser/"
 
-    def download(self):
-        if os.path.exists(os.path.join(self.model_dir, os.path.basename(self.url))):
-            logger.info(f" - {os.path.basename(self.url)} already downloaded")
+    def download(self, url):
+        if os.path.exists(os.path.join(self.model_dir, os.path.basename(url))):
+            logger.info(f" - {os.path.basename(url)} already downloaded")
         else:
-            logger.info(f" - Downloading {os.path.basename(self.url)}")
-            response = requests.get(self.url, stream=True)
-            with open(os.path.join(self.model_dir, os.path.basename(self.url)), "wb") as f:
+            logger.info(f" - Downloading {os.path.basename(url)}")
+            response = requests.get(url, stream=True)
+            with open(os.path.join(self.model_dir, os.path.basename(url)), "wb") as f:
                 for chunk in response.iter_content(chunk_size=1024):
                     f.write(chunk)
 
     def download_laser2(self):
         self.download(f"{self.url}/laser2.pt")
         self.download(f"{self.url}/laser2.spm")
-        self.download(f"{self.url}laser2.cvocab")
+        self.download(f"{self.url}/laser2.cvocab")
 
     def download_laser3(self, lang, version="v1", spm=False):
-        file_name = f"laser3-{lang}.{version}.pt"
+        if lang not in LANGUAGE_MAPPING:
+            logger.info(f"Unsupported language name: {lang}. Please specify a supported language name.")
+            return
+    
+        if len(LANGUAGE_MAPPING[lang]) > 1:
+            options = ', '.join(f"'{opt}'" for opt in LANGUAGE_MAPPING[lang])
+            logger.info(f"Language '{lang}' has multiple options: {options}. Please specify.")
+            return
+
+        lang = LANGUAGE_MAPPING[lang]
+        file_name = f"{self.url}/laser3-{lang}.{version}.pt"
         self.download(file_name)
         if spm:
-            file_name = f"laser3-{lang}.{version}.spm"
-            self.download(file_name)
-            file_name = f"laser3-{lang}.{version}.cvocab"
-            self.download(file_name)
+            if lang in SPM_LANGUAGE:
+                file_name = f"{self.url}/laser3-{lang}.{version}.spm"
+                self.download(file_name)
+                file_name = f"{self.url}/laser3-{lang}.{version}.cvocab"
+                self.download(file_name)
+            else:
+                self.download(f"{self.url}/laser2.spm")
+                self.download(f"{self.url}/laser2.cvocab")
 
-    def main(args):
-        model_dir = args.model_dir or os.path.expanduser("~/.cache/laser_encoders")
-        downloader = LaserModelDownloader(model_dir)
-
+    def main(self, args):
         if args.laser == "laser2":
-            downloader.download_laser2()
+            self.download_laser2()
         elif args.laser == "laser3":
-            lang = args.lang
-            version = args.version
-            spm = args.spm
-            downloader.download_laser3(lang, version, spm)
+            self.download_laser3(lang=args.lang, version=args.version, spm=args.spm)
         else:
-            raise ValueError("Please specify either --laser. either laeser2 or laser3")
+            logger.info("Please specify --laser. either laeser2 or laser3")
+            return
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="LASER: Embed sentences")
-    parser.add_argument("--laser", action="store_true", help="LASER model to download")
-    parser.add_argument("--lang", type=str, help="The language name in FLORES200 format")
+    parser = argparse.ArgumentParser(description="LASER: Download Laser models")
+    parser.add_argument("--laser", help="LASER model to download")
+    parser.add_argument("--lang", type=str, required=True, help="The language name in FLORES200 format")
     parser.add_argument("--version", type=str, help="The encoder model version")
-    parser.add_argument("--spm", action="store_true", help="Download the SPM model as well")
+    parser.add_argument("--spm", help="Download the SPM model as well?")
+    parser.add_argument("--model_dir", type=str, help="The directory to download the models to")
     args = parser.parse_args()
-    laser_downloader = LaserModelDownloader(version=1, model_dir=os.path.expanduser("~/.cache/laser_encoders"))
-    laser_downloader.main(args)
+    model_dir = args.model_dir or os.path.expanduser("~/.cache/laser_encoders")
+    downloader = LaserModelDownloader(model_dir)
+    downloader.main(args)
---
+++ laser_encoders/language_list.py
@@ -0,0 +1,32 @@
+
+# Language mapping to handle different language codes and names
+LANGUAGE_MAPPING = {
+  'central kanuri': ('knc_Latn', 'knc_Arab'),
+  'knc': ('knc_Latn', 'knc_Arab'),
+  'knc_Latn': 'knc_Latn'
+}
+# LANGUAGE_MAPPING = {
+#     ('french', 'fra', 'fr', 'fra_Latn'): 'fra_Latn',
+#     ('english', 'eng', 'en', 'eng_Latn'): 'eng_Latn',
+# }
+
+# langs = ["aka_Latn", "als_Latn", "amh_Ethi", "asm_Beng", "awa_Deva", "ayr_Latn", "azb_Arab", "azj_Latn", "bak_Cyrl", "bam_Latn", "ban_Latn", "bel_Cyrl",
+#         "bem_Latn", "ben_Beng", "bho_Deva", "bjn_Latn", "bod_Tibt", "bug_Latn", "ceb_Latn", "cjk_Latn", "ckb_Arab", "crh_Latn", "cym_Latn", "dik_Latn", "diq_Latn", 
+#         "dyu_Latn", "dzo_Tibt", "ewe_Latn", "fao_Latn", "fij_Latn", "fon_Latn", "fur_Latn", "fuv_Latn", "gaz_Latn", "gla_Latn", "gle_Latn", "grn_Latn", "guj_Gujr", 
+#         "hat_Latn", "hau_Latn", "hin_Deva", "hne_Deva", "hye_Armn", "ibo_Latn", "ilo_Latn", "ind_Latn", "jav_Latn", "kab_Latn", "kac_Latn", "kam_Latn", "kan_Knda", 
+#         "kas_Arab", "kas_Deva", "kat_Geor", "kaz_Cyrl", "kbp_Latn", "kea_Latn", "khk_Cyrl", "khm_Khmr", "kik_Latn", "kin_Latn", "kir_Cyrl", "kmb_Latn", "kmr_Latn", 
+#         "knc_Arab", "knc_Latn", "kon_Latn", "lao_Laoo", "lij_Latn", "lim_Latn", "lin_Latn", "lmo_Latn", "ltg_Latn", "ltz_Latn", "lua_Latn", "lug_Latn", "luo_Latn", 
+#         "lus_Latn", "mag_Deva", "mai_Deva", "mal_Mlym", "mar_Deva", "min_Latn", "mlt_Latn", "mni_Beng", "mos_Latn", "mri_Latn", "mya_Mymr", "npi_Deva", "nso_Latn", 
+#         "nus_Latn", "nya_Latn", "ory_Orya", "pag_Latn", "pan_Guru", "pap_Latn", "pbt_Arab", "pes_Arab", "plt_Latn", "prs_Arab", "quy_Latn", "run_Latn", "sag_Latn", 
+#         "san_Deva", "sat_Beng", "scn_Latn", "shn_Mymr", "sin_Sinh", "smo_Latn", "sna_Latn", "snd_Arab", "som_Latn", "sot_Latn", "srd_Latn", "ssw_Latn", "sun_Latn",
+#         "swh_Latn", "szl_Latn", "tam_Taml", "taq_Latn", "tat_Cyrl", "tel_Telu", "tgk_Cyrl", "tgl_Latn", "tha_Thai", "tir_Ethi", "tpi_Latn", "tsn_Latn", "tso_Latn", 
+#         "tuk_Latn", "tum_Latn", "tur_Latn", "twi_Latn", "tzm_Tfng", "uig_Arab", "umb_Latn", "urd_Arab", "uzn_Latn", "vec_Latn", "war_Latn", "wol_Latn", "xho_Latn",
+#         "ydd_Hebr", "yor_Latn", "zsm_Latn", "zul_Latn"]
+
+
+SPM_LANGUAGE = ["amh_Ethi", "ayr_Latn", "azj_Latn", "bak_Cyrl", "bel_Cyrl", "bod_Tibt", "ckb_Arab", "crh_Latn", "dik_Latn", 
+                 "dzo_Tibt", "fur_Latn", "fuv_Latn", "grn_Latn", "kab_Latn", "kac_Latn", "kaz_Cyrl", "kir_Cyrl", "kmr_Latn", 
+                 "lij_Latn", "lim_Latn", "lmo_Latn", "ltg_Latn",
+           "mya_Mymr" "pbt_Arab" "pes_Arab", "prs_Arab", "sat_Beng", "scn_Latn", "srd_Latn", "szl_Latn", "taq_Latn", "tgk_Cyrl", "tir_Ethi",
+           "tzm_Tfng", "vec_Latn"
+]
