1a9937c208ee3b08e410f8ae26c60a160a0e1252
paul <okewunmipaul@yahoo.com>
2023-09-30 01:46:53 +0100

    style: Format code and sort imports using black and isort

--- docker/app.py
+++ docker/app.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import os
 import socket
+
+import numpy as np
 from flask import Flask, jsonify, request
 from laser_encoders import initialize_encoder, initialize_tokenizer
-import numpy as np
 
 app = Flask(__name__)
 
@@ -12,17 +13,19 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     print("/")
-    html = "<h3>Hello {name}!</h3>" \
-            "<b>Hostname:</b> {hostname}<br/>"
+    html = "<h3>Hello {name}!</h3>" "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("LASER", "world"), hostname=socket.gethostname())
+
 
 @app.route("/vectorize", methods=["GET"])
 def vectorize():
-    content = request.args.get('q')
-    lang = request.args.get('lang', 'en')  # Default to English if 'lang' is not provided
+    content = request.args.get("q")
+    lang = request.args.get(
+        "lang", "en"
+    )  # Default to English if 'lang' is not provided
 
     if content is None:
-        return jsonify({'error': 'Missing input content'})
+        return jsonify({"error": "Missing input content"})
 
     encoder = initialize_encoder(lang=lang)
     tokenizer = initialize_tokenizer(lang=lang)
@@ -34,9 +37,9 @@ def vectorize():
     embeddings = encoder.encode_sentences([tokenized_sentence])
     embeddings_list = embeddings.tolist()
 
-    body = {'content': content, 'embedding': embeddings_list}
+    body = {"content": content, "embedding": embeddings_list}
     return jsonify(body)
 
-if __name__ == "__main__":
-    app.run(debug=True, port=80, host='0.0.0.0')
 
+if __name__ == "__main__":
+    app.run(debug=True, port=80, host="0.0.0.0")
