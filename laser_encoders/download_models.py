d13174b811c9ff8295809de7870c9a12edb66463
paul <okewunmipaul@yahoo.com>
2023-10-22 16:08:08 +0100

    Remove 'tokenize' argument from initialize_encoder function

--- laser_encoders/download_models.py
+++ laser_encoders/download_models.py
@@ -126,7 +126,6 @@ def initialize_encoder(
     model_dir: str = None,
     spm: bool = True,
     laser: str = None,
-    tokenize: bool = False,
 ):
     downloader = LaserModelDownloader(model_dir)
     if laser is not None:
@@ -157,17 +156,13 @@ def initialize_encoder(
     model_dir = downloader.model_dir
     model_path = os.path.join(model_dir, f"{file_path}.pt")
     spm_vocab = os.path.join(model_dir, f"{file_path}.cvocab")
-    spm_model = None
+
     if not os.path.exists(spm_vocab):
         # if there is no cvocab for the laser3 lang use laser2 cvocab
         spm_vocab = os.path.join(model_dir, "laser2.cvocab")
-    if tokenize:
-        spm_model = os.path.join(model_dir, f"{file_path}.spm")
-        if not os.path.exists(spm_model):
-            spm_model = os.path.join(model_dir, "laser2.spm")
-
+    
     return SentenceEncoder(
-        model_path=model_path, spm_vocab=spm_vocab, spm_model=spm_model
+        model_path=model_path, spm_vocab=spm_vocab, spm_model=None
     )
 
 
