429a908a09addc965ac4589d1c42f4b67f54f838
paul <okewunmipaul@yahoo.com>
2023-11-25 19:11:06 +0100

    Create cache for encoder to improve subsequent request speed

--- docker/app.py
+++ docker/app.py
@@ -3,19 +3,19 @@
 import os
 import socket
 
-import numpy as np
 from flask import Flask, jsonify, request
 from laser_encoders import LaserEncoderPipeline
 
 app = Flask(__name__)
 
+# Global cache for encoders
+encoder_cache = {}
 
 @app.route("/")
 def root():
     print("/")
     html = "<h3>Hello {name}!</h3>" "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("LASER", "world"), hostname=socket.gethostname())
-
 
 @app.route("/vectorize", methods=["GET"])
 def vectorize():
@@ -26,7 +26,11 @@ def vectorize():
         return jsonify({"error": "Missing input content"}), 400
 
     try:
-        encoder = LaserEncoderPipeline(lang=lang)
+        # Use cached encoder if available, else create a new one
+        if lang not in encoder_cache:
+            encoder_cache[lang] = LaserEncoderPipeline(lang=lang)
+        encoder = encoder_cache[lang]
+
         embeddings = encoder.encode_sentences([content])
         embeddings_list = embeddings.tolist()
         body = {"content": content, "embedding": embeddings_list}
@@ -38,7 +42,6 @@ def vectorize():
             return jsonify({"error": f"Language '{lang}' is not supported."}), 400
         else:
             return jsonify({"error": str(e)}), 400
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
