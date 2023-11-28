f51fb0a51efe8218bdc5f4fda7c6fc4e462e0c63
paul <okewunmipaul@yahoo.com>
2023-11-28 13:23:11 +0100

    Improve caching for laser2 languages

--- docker/app.py
+++ docker/app.py
@@ -4,12 +4,15 @@ import os
 import socket
 
 from flask import Flask, jsonify, request
+
 from laser_encoders import LaserEncoderPipeline
+from laser_encoders.language_list import LASER2_LANGUAGES_LIST
 
 app = Flask(__name__)
 
 # Global cache for encoders
 encoder_cache = {}
+
 
 @app.route("/")
 def root():
@@ -17,19 +20,36 @@ def root():
     html = "<h3>Hello {name}!</h3>" "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("LASER", "world"), hostname=socket.gethostname())
 
+
 @app.route("/vectorize", methods=["GET"])
 def vectorize():
     content = request.args.get("q")
-    lang = request.args.get("lang", "eng")  # Default to English if 'lang' is not provided
+    lang = request.args.get(
+        "lang", "eng"
+    )  # Default to English if 'lang' is not provided
 
     if content is None:
         return jsonify({"error": "Missing input content"}), 400
 
     try:
-        # Use cached encoder if available, else create a new one
-        if lang not in encoder_cache:
+        lang_prefix = lang[:3]
+
+        # Find if lang uses laser2 encoder and is already cached
+        cached_laser2_language = next(
+            (
+                l
+                for l in LASER2_LANGUAGES_LIST
+                if l.startswith(lang_prefix) and l in encoder_cache
+            ),
+            None,
+        )
+
+        if cached_laser2_language:
+            encoder = encoder_cache[cached_laser2_language]
+        else:
+            # Use the provided language code as is for the new encoder
             encoder_cache[lang] = LaserEncoderPipeline(lang=lang)
-        encoder = encoder_cache[lang]
+            encoder = encoder_cache[lang]
 
         embeddings = encoder.encode_sentences([content])
         embeddings_list = embeddings.tolist()
@@ -42,6 +62,7 @@ def vectorize():
             return jsonify({"error": f"Language '{lang}' is not supported."}), 400
         else:
             return jsonify({"error": str(e)}), 400
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
